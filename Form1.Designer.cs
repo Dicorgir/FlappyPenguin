37f5d7dbc94f819814374676256cc9f5df8ab7a7
Diego Cornejo <dcornejogiraldo@safareyes.es>
2024-06-24 19:31:50 +0200

    Cargando Juego Flappy Penguin

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿using System;
+
+namespace FlappyPenguin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.TuboArriba = new System.Windows.Forms.PictureBox();
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.TuboAbajo = new System.Windows.Forms.PictureBox();
+            this.Barrera = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.Puntaje = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.TuboArriba)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TuboAbajo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Barrera)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TuboArriba
+            // 
+            this.TuboArriba.BackColor = System.Drawing.Color.Transparent;
+            this.TuboArriba.Image = global::FlappyPenguin.Properties.Resources.tuberia21;
+            this.TuboArriba.Location = new System.Drawing.Point(143, -98);
+            this.TuboArriba.Name = "TuboArriba";
+            this.TuboArriba.Size = new System.Drawing.Size(111, 400);
+            this.TuboArriba.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.TuboArriba.TabIndex = 0;
+            this.TuboArriba.TabStop = false;
+            // 
+            // Player
+            // 
+            this.Player.BackColor = System.Drawing.Color.Transparent;
+            this.Player.Image = global::FlappyPenguin.Properties.Resources.pinguino1;
+            this.Player.Location = new System.Drawing.Point(43, 226);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(83, 76);
+            this.Player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Player.TabIndex = 1;
+            this.Player.TabStop = false;
+            // 
+            // TuboAbajo
+            // 
+            this.TuboAbajo.BackColor = System.Drawing.Color.Transparent;
+            this.TuboAbajo.Image = global::FlappyPenguin.Properties.Resources.tuberia3;
+            this.TuboAbajo.Location = new System.Drawing.Point(143, 359);
+            this.TuboAbajo.Name = "TuboAbajo";
+            this.TuboAbajo.Size = new System.Drawing.Size(111, 237);
+            this.TuboAbajo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.TuboAbajo.TabIndex = 2;
+            this.TuboAbajo.TabStop = false;
+            // 
+            // Barrera
+            // 
+            this.Barrera.Image = global::FlappyPenguin.Properties.Resources.franja;
+            this.Barrera.Location = new System.Drawing.Point(-27, 420);
+            this.Barrera.Name = "Barrera";
+            this.Barrera.Size = new System.Drawing.Size(1616, 33);
+            this.Barrera.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Barrera.TabIndex = 3;
+            this.Barrera.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 1;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Puntaje
+            // 
+            this.Puntaje.AutoSize = true;
+            this.Puntaje.Location = new System.Drawing.Point(76, 207);
+            this.Puntaje.Name = "Puntaje";
+            this.Puntaje.Size = new System.Drawing.Size(14, 16);
+            this.Puntaje.TabIndex = 4;
+            this.Puntaje.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(307, 456);
+            this.Controls.Add(this.Puntaje);
+            this.Controls.Add(this.TuboAbajo);
+            this.Controls.Add(this.Player);
+            this.Controls.Add(this.TuboArriba);
+            this.Controls.Add(this.Barrera);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Flappy Penguin";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.TuboArriba)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TuboAbajo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Barrera)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox TuboArriba;
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.PictureBox TuboAbajo;
+        private System.Windows.Forms.PictureBox Barrera;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label Puntaje;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace FlappyPenguin
+{
+    public partial class Form1 : Form
+    {
+        // Declaración de variables
+        int ContadorMovimiento = 1;  // Contador para la animación del jugador
+        bool VolarArriba = false;   // Bandera para controlar el vuelo hacia arriba del jugador
+        int Distancia = 0;   // Distancia inicial de los tubos respecto al borde superior del formulario
+        Random PosicionRandom = new Random();   // Generador de números aleatorios para posiciones de tubos
+        int distanciaEntreTubos = 150; // Espacio vertical entre los tubos
+        bool haPasadoElTubo = false; // Bandera para controlar si el jugador ha pasado completamente el tubo
+
+        // Velocidad inicial y máxima del jugador
+        int velocidadHorizontal = 3;
+        int velocidadMaxima = 6;
+
+        // Constructor de la clase Form1
+        public Form1()
+        {
+            InitializeComponent();  // Inicializa los componentes del formulario
+            this.KeyPreview = true;  // Permite que el formulario maneje los eventos de teclado directamente
+            IniciarJuego();  // Inicia el juego configurando los componentes y variables
+
+            // Configuración del formulario
+            this.FormBorderStyle = FormBorderStyle.FixedSingle; // Borde del formulario fijo
+            this.MaximizeBox = false; // Botón de maximizar deshabilitado
+            this.MinimizeBox = false; // Botón de minimizar deshabilitado
+        }
+
+        // Método para inicializar el juego
+        public void IniciarJuego()
+        {
+            Player.Location = new Point(19, 225); // Posición inicial del jugador
+            // Genera un valor aleatorio para la posición vertical de los tubos
+            Distancia = PosicionRandom.Next(50, this.ClientSize.Height - distanciaEntreTubos - 50);
+            TuboArriba.Location = new Point(270, -173 - Distancia); // Posición inicial del tubo superior
+            TuboAbajo.Location = new Point(270, TuboArriba.Location.Y + TuboArriba.Height + distanciaEntreTubos); // Posición inicial del tubo inferior
+            Puntaje.Text = "0"; // Inicializa el puntaje en cero
+            haPasadoElTubo = false; // Reinicia la bandera de control de paso de tubo al iniciar juego
+            velocidadHorizontal = 3; // Reinicia la velocidad horizontal al inicio del juego
+        }
+
+        // Método que se ejecuta cada cierto intervalo de tiempo (timer1_Tick)
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int CantidadMovimientos = 5; // Número de imágenes en la animación del jugador
+
+            // Lógica para cambiar la imagen del jugador según el contador de movimientos
+            if (ContadorMovimiento <= CantidadMovimientos)
+            {
+                Player.Image = Properties.Resources.pinguino1;
+                ContadorMovimiento++;
+            }
+            if ((ContadorMovimiento > CantidadMovimientos / 2) && (ContadorMovimiento <= CantidadMovimientos * 2))
+            {
+                Player.Image = Properties.Resources.pinguino1;
+                ContadorMovimiento++;
+            }
+            ContadorMovimiento = (ContadorMovimiento > CantidadMovimientos * 2) ? 0 : ContadorMovimiento;
+
+            int ly = Player.Location.Y; // Posición vertical actual del jugador
+
+            // Lógica para el movimiento vertical del jugador
+            if (VolarArriba)
+            {
+                ly = ly - 15; // Mueve hacia arriba cuando VolarArriba es true
+                VolarArriba = false; // Reinicia la bandera después de mover hacia arriba
+            }
+            else
+            {
+                ly += 2; // Incremento de posición hacia abajo (gravedad simulada)
+            }
+
+            // Limita el movimiento del jugador dentro del formulario
+            ly = Math.Max(0, Math.Min(this.ClientSize.Height - Player.Height, ly));
+            Player.Location = new Point(Player.Location.X, ly);
+
+            // Verifica colisiones con los elementos del juego y reinicia el juego si hay colisión
+            if (Player.Bounds.IntersectsWith(Barrera.Bounds) || Player.Bounds.IntersectsWith(TuboArriba.Bounds) || Player.Bounds.IntersectsWith(TuboAbajo.Bounds))
+            {
+                IniciarJuego();
+            }
+
+            Puntaje.Location = new Point(Player.Location.X + 30, Player.Location.Y - 25); // Posición del texto de puntaje
+
+            // Incrementa la puntuación si el jugador pasa completamente el tubo
+            if (Player.Right >= TuboArriba.Left && Player.Left <= TuboArriba.Right)
+            {
+                if (!haPasadoElTubo)
+                {
+                    Puntaje.Text = (int.Parse(Puntaje.Text) + 1).ToString();
+                    haPasadoElTubo = true;
+                }
+            }
+            else
+            {
+                haPasadoElTubo = false;
+            }
+
+            // Aumenta la velocidad horizontal del juego progresivamente
+            if (velocidadHorizontal < velocidadMaxima)
+            {
+                velocidadHorizontal += 1; // Ajustable según preferencia
+            }
+        }
+
+        // Evento de tecla presionada en el formulario
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Space)
+            {
+                VolarArriba = true; // Activa el vuelo hacia arriba cuando se presiona la tecla Espacio
+            }
+        }
+
+        // Timer para mover los tubos horizontalmente
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            // Mueve los tubos hacia la izquierda mientras estén dentro de los límites del formulario
+            if (TuboAbajo.Location.X > -TuboAbajo.Width)
+            {
+                TuboAbajo.Location = new Point(TuboAbajo.Location.X - velocidadHorizontal, TuboAbajo.Location.Y);
+                TuboArriba.Location = new Point(TuboArriba.Location.X - velocidadHorizontal, TuboArriba.Location.Y);
+            }
+            else
+            {
+                // Reposiciona los tubos y genera nueva posición vertical para el próximo paso
+                Distancia = PosicionRandom.Next(50, this.ClientSize.Height - distanciaEntreTubos - 50);
+                TuboArriba.Location = new Point(this.ClientSize.Width, -173 - Distancia);
+                TuboAbajo.Location = new Point(this.ClientSize.Width, TuboArriba.Location.Y + TuboArriba.Height + distanciaEntreTubos);
+            }
+        }
+
+        // Timer para mover la barrera horizontalmente (en caso de que haya una)
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            // Mueve la barrera hacia la izquierda mientras esté dentro de los límites del formulario
+            Barrera.Location = (Barrera.Location.X > -Barrera.Width) ? new Point(Barrera.Location.X - velocidadHorizontal, Barrera.Location.Y) : new Point(this.ClientSize.Width, Barrera.Location.Y);
+        }
+    }
+}
